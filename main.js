97ff3765623fe703381e4696d836e9577e167b69
ngotaiduy <40078604+ngotaiduy@users.noreply.github.com>
2021-05-22 21:16:19 +0700

    Upload web

---
+++ main.js
@@ -0,0 +1,7 @@
+// var isChrome =
+//   /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// if (!isChrome) {
+//   $("#iframeAudio").remove();
+// } else {
+//   $("#playAudio").remove();
+// }
